6ee9de047265e5f0e00dacee0b698e6afc41ea60
Eric Lau <44459660+erxclau@users.noreply.github.com>
2022-02-20 00:57:26 -0500

    Node engine (#4)
    * Change engine to node for env vars
    
    * Add parcel engine

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
-var $2E0hp$parcelplugin = require("@parcel/plugin");
-var $2E0hp$nunjucks = require("nunjucks");
+var $4BP8v$parcelplugin = require("@parcel/plugin");
+var $4BP8v$nunjucks = require("nunjucks");
 
 function $parcel$defineInteropFlag(a) {
   Object.defineProperty(a, '__esModule', {value: true, configurable: true});
@@ -10,11 +10,12 @@ function $parcel$export(e, n, v, s) {
 
 $parcel$defineInteropFlag(module.exports);
 
-$parcel$export(module.exports, "default", () => $9fc92474a186eb2d$export$2e2bcd8739ae039);
+$parcel$export(module.exports, "default", () => $f7f4a66df2960fae$export$2e2bcd8739ae039);
 
 
-var $9fc92474a186eb2d$export$2e2bcd8739ae039 = new $2E0hp$parcelplugin.Transformer({
+var $f7f4a66df2960fae$export$2e2bcd8739ae039 = new $4BP8v$parcelplugin.Transformer({
     async loadConfig ({ config: config  }) {
+        var ref1;
         // load config file
         const { contents: contents  } = await config.getConfig([
             "config.json"
@@ -23,24 +24,27 @@ var $9fc92474a186eb2d$export$2e2bcd8739ae039 = new $2E0hp$parcelplugin.Transform
         let archie = null;
         const confaml = contents.fetch.filter((d)=>!d.hasOwnProperty("sheetId")
         );
-        if (confaml.length === 1) archie = (await config.getConfig([
+        if (confaml.length === 1) archie = (ref1 = await config.getConfig([
             confaml[0].output
-        ]))?.contents;
-        else if (confaml.length > 1) archie = await Promise.all(confaml.map(async (aml)=>(await config.getConfig([
+        ])) === null || ref1 === void 0 ? void 0 : ref1.contents;
+        else if (confaml.length > 1) archie = await Promise.all(confaml.map(async (aml)=>{
+            var ref;
+            return (ref = await config.getConfig([
                 aml.output
-            ]))?.contents
-        ));
+            ])) === null || ref === void 0 ? void 0 : ref.contents;
+        }));
         return {
             config: contents,
+            env: process.env.NODE_ENV,
             archie: archie
         };
     },
     async transform ({ asset: asset , config: config  }) {
-        $2E0hp$nunjucks.configure({
+        $4BP8v$nunjucks.configure({
             autoescape: false
         });
         let code = await asset.getCode();
-        let result = $2E0hp$nunjucks.renderString(code, config);
+        let result = $4BP8v$nunjucks.renderString(code, config);
         asset.setCode(result);
         return [
             asset
